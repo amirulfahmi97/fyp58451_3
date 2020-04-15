2b69de793c8b9a301099a204e002d66150dabcc2
amirulfahmi97 <38986749+amirulfahmi97@users.noreply.github.com>
2020-04-15 22:40:50 +0800

    15 april first try using javascript

--- app/Http/Controllers/SubjectPagesController.php
+++ app/Http/Controllers/SubjectPagesController.php
@@ -9,6 +9,7 @@ use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Facades\DB;
 
 class SubjectPagesController extends Controller
 {
@@ -20,8 +21,8 @@ class SubjectPagesController extends Controller
     public function show($id,$subjectcode){
         //$userfile= TeacherFile::where('user_id',$user_id)->first();
         //$username= User::where('login_userid',$user_id)->first();
-         $id = Crypt::decrypt($id);
-      //   echo "<br> ".$id;
+        $id = Crypt::decrypt($id);
+        //   echo "<br> ".$id;
         //echo "<br>".$subjectcode;
         //echo "<br>";
 
@@ -29,26 +30,35 @@ class SubjectPagesController extends Controller
         $subject = new SubjectFile();
         $users = SubjectFile::where('subjectCode',$subjectcode)->first();
         //echo "Subject Code :".$users->subjectCode;
-       // echo "<br>Subject ID :".$users->id . "With ID: ";
+        // echo "<br>Subject ID :".$users->id . "With ID: ";
+        $subjectid = $users->id;
 
-        $homework = HomeworkFile::where('subjectid',$users->id)->first();
+        $homework = HomeworkFile::where('subjectid',$users->id)->get();
+        $homework2 = DB::table('homework_files')->where('subjectid',$subjectid)->get();
         $hw = HomeworkFile::all();
-         $hwork = $hw->toArray();
+        $hwork = $homework->toArray();
 
-       // echo $homework->id;
-        $newdate =  $homework->dateline;
-       // echo "<br>".date('F',strtotime($newdate));
-        //echo "<br>";
-            $month = date('F',strtotime($newdate));
-        //echo $result = Carbon::parse($homework->dateline)->format('d/m/Y');
+        if($homework2){
+            return view('teacher.subject.index')->with(compact('users','homework','hw','hwork'));
+        }
+        else{
+            //$homework == null;
+            echo "noting in here";
+            return view('teacher.subject.backup.index')->with(compact('users','homework','hw'));
 
-        function january(){
-            echo "something something";
+
         }
 
-      //  return view('teacher.subject.2020calendar')->with(compact('users','month','homework'));
+        // echo $homework->id;
+       // $newdate =  $homework->dateline;
+        // echo "<br>".date('F',strtotime($newdate));
+        //echo "<br>";
+      //  $month = date('F',strtotime($newdate));
+        //echo $result = Carbon::parse($homework->dateline)->format('d/m/Y');
 
-        return view('teacher.subject.index')->with(compact('users','month','homework','hw','hwork'));
+
+        //  return view('teacher.subject.2020calendar')->with(compact('users','month','homework'));
+
 
     }
 
--- resources/views/teacher/subject/backup/2020calendar.blade.php
+++ resources/views/teacher/subject/backup/2020calendar.blade.php
@@ -1,66 +1,5 @@
 
-<p id="demo"></p>
 
-Looping ID<p id="id1" ></p>
-TEST ID HOMEWORK<p id="test"></p>
-TOTAL OBJECT<p id="count"></p>
-<div id="output" class="out">
-
-</div>
-<script type="text/javascript">
-
-
-var users = {!! json_encode($hwork) !!};
-//above is javascript object that receive from the server and below we con
-//below converted to json object
-var jsonarray = JSON.stringify(users);
-//below converted back to javscript object
-var jsonobject = jQuery.parseJSON(jsonarray);
-var count = Object.keys(jsonobject).length;
-//console.log(test);
-document.getElementById("demo").innerHTML = jsonarray;
-document.getElementById("count").innerHTML = count;
-
-document.getElementById("test").innerHTML = jsonobject[0].id;
-
-
-var test4 = jQuery.parseJSON(jsonarray);
-
-$.each(test4,function (key,value) {
-
-    document.getElementById("id1").innerHTML += value.id;
-
-
-})
-
-   ///dah siap skit, just nk update skit and everything looking good i guess.
-   //later upload to github
-
-
-
-
-window.onload=function(){
-    var output = document.getElementById('output');
-    var i=1;
-    var val="";
-    while(i<=9)
-    {
-
-        if(!document.getElementById('timedrpact'+i))
-        {
-            var ele = document.createElement("div");
-            ele.setAttribute("id","timedrpact"+i);
-            ele.setAttribute("class","inner");
-            ele.innerHTML="test"+i;
-            output.appendChild(ele);
-
-        }
-        i++;
-
-
-    }
-};
-</script>
 
 
 <div class="container-fluid">
@@ -70,7 +9,6 @@ window.onload=function(){
             <a>test</a>
 
 
-
         </div>
     </div>
 
@@ -78,6 +16,8 @@ window.onload=function(){
     <div class="row  border border-dark border-bottom-0 rounded m-2 mb-4 pb-4">
         <div class="col-md-6">
             <h3 class=" pt-3 pb-3">February</h3>
+          /
+            test
 
             <a>test</a>
         </div>
