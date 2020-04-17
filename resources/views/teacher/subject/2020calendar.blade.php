5c4a94e234a9aa0b8bcebbfcd38fef5430d7058e
amirulfahmi97 <38986749+amirulfahmi97@users.noreply.github.com>
2020-04-18 01:40:18 +0800

    18 april second try using javascript, working as hell, just nnti aturkan balik, and reuse redundency code .

--- resources/views/teacher/subject/2020calendar.blade.php
+++ resources/views/teacher/subject/2020calendar.blade.php
@@ -4,9 +4,13 @@
 Looping ID<p id="id1" ></p>
 TEST ID HOMEWORK<p id="test"></p>
 TOTAL OBJECT<p id="count"></p>
+<div id="div1"></div>
+<div id="div3"></div>
 <div id="output" class="out">
 
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript">
 
 
@@ -21,53 +25,169 @@ var count = Object.keys(jsonobject).length;
 document.getElementById("demo").innerHTML = jsonarray;
 document.getElementById("count").innerHTML = count;
 
-document.getElementById("test").innerHTML = jsonobject[0].id;
+document.getElementById("test").innerHTML = jsonobject[1].id;
 
 
-var test4 = jQuery.parseJSON(jsonarray);
-
-$.each(test4,function (key,value) {
-
-    document.getElementById("id1").innerHTML += value.id;
-
-
-})
-
-   ///dah siap skit, just nk update skit and everything looking good i guess.
-   //later upload to github
-
-
-
+var div1 = document.getElementById("div1");
+var div3 = document.getElementById("div3");
 
 window.onload=function(){
-    var output = document.getElementById('output');
-    var i=1;
-    var val="";
-    while(i<=9)
-    {
+function refactorHomework(homeworkString){
+    var homeworkDiv = document.getElementById("div3");
+    homeworkDiv.innerHTML="Homework List as Below";
 
-        if(!document.getElementById('timedrpact'+i))
+
+    homeworkString.forEach(function (test) {
+        var homeworkdiv = document.createElement("div"),
+            iddiv= document.createElement("div"),
+            contentdiv = document.createElement("div"),
+            testjak = document.createElement("div"),
+            br = document.createElement("br");
+
+
+        var homeworkDateCreate = document.createElement("div");
+        var monthCreated = document.createElement("div");
+
+       // homeworkdiv.innerHTML="Homework list";
+        testjak.innerHTML = jsonobject[0].subjectid;
+        iddiv.innerHTML = "Id is = " + test.id;
+        contentdiv.innerHTML = "Description is " +  test.homeworkDesc;
+        homeworkDateCreate.innerHTML = "Data Created at " + test.created_at;
+
+        var test2 = test.created_at;
+
+        var t = test2.split(/[- :]/);
+        var d = new Date(Date.UTC(t[0], t[1]-1, t[2], t[3], t[4], t[5]));
+        var a = d.getMonth();
+
+        switch (a) {
+            case 0:
+                //
+                break;
+            case 1:
+                //
+                break;
+            case 2:
+                //
+                break;
+            case 3:
+                //
+                break;
+            case 4:
+                //
+                break;
+            case 5:
+                //
+                break;
+            case 6:
+                //
+                break;
+            case 7:
+                //
+                break;
+            case 8:
+                //
+                break;
+            case 9:
+                //
+                break;
+            case 10:
+                //
+                break;
+            case 11:
+                //
+                break;
+        }
+        iddiv.setAttribute("class","testname");
+        homeworkdiv.className="bigdiv";
+        // contentdiv.innerHTML="Description";
+        div3.style.color="blue";
+
+        if(a === 0)
         {
-            var ele = document.createElement("div");
-            ele.setAttribute("id","timedrpact"+i);
-            ele.setAttribute("class","inner");
-            ele.innerHTML="test"+i;
-            output.appendChild(ele);
+            monthCreated.innerHTML="January";
+            homeworkdiv.appendChild(br);
+            homeworkdiv.appendChild(iddiv);
+            homeworkdiv.appendChild(contentdiv);
+            homeworkdiv.appendChild(homeworkDateCreate);
+            homeworkdiv.appendChild(monthCreated);
+            var jan = document.getElementById("jan");
+            jan.appendChild(homeworkdiv);
 
         }
-        i++;
+       else if ( a === 2){
+            monthCreated.innerHTML = "month created is March";
+            homeworkdiv.appendChild(br);
+            homeworkdiv.appendChild(iddiv);
+            homeworkdiv.appendChild(contentdiv);
+            homeworkdiv.appendChild(homeworkDateCreate);
+            homeworkdiv.appendChild(monthCreated);
+            var march = document.getElementById("march");
+            march.appendChild(homeworkdiv);
+
+        }
+        else if (a === 3)
+        {
+            monthCreated.innerHTML = "month created is April";
+            homeworkdiv.appendChild(br);
+            homeworkdiv.appendChild(iddiv);
+            homeworkdiv.appendChild(contentdiv);
+            homeworkdiv.appendChild(homeworkDateCreate);
+            homeworkdiv.appendChild(monthCreated);
+            var april = document.getElementById("april");
+            april.appendChild(homeworkdiv);
+        }
+         else {
+            homeworkdiv.appendChild(br);
+            homeworkdiv.appendChild(iddiv);
+            homeworkdiv.appendChild(contentdiv);
+            homeworkdiv.appendChild(homeworkDateCreate);
+            homeworkdiv.appendChild(monthCreated);
+
+            div3.appendChild(homeworkdiv);
+             monthCreated.innerHTML="besides that";
+         }
 
 
-    }
+
+
+     //   homeworkdiv.appendChild(br);
+     //   homeworkdiv.appendChild(iddiv);
+     //   homeworkdiv.appendChild(contentdiv);
+     //   homeworkdiv.appendChild(homeworkDateCreate);
+     //   homeworkdiv.appendChild(monthCreated);
+     //
+     //   div3.appendChild(homeworkdiv);
+
+    })
+
+}
+  function getHomeworkstring(callback){
+
+      var homeworkArray = jsonobject;
+      callback(homeworkArray);
+
+  }
+  getHomeworkstring(function (homeworkArray) {
+      refactorHomework(homeworkArray);
+  })
+
+
+
 };
+
+
 </script>
 
 
 <div class="container-fluid">
     <div class="row border border-dark border-bottom-0 rounded m-2 mb-4 pb-4">
         <div class="col-md-6">
+            <div id="jan">
+
             <h3 class=" pt-3 pb-3">January</h3>
             <a>test</a>
+            </div>
 
 
 
@@ -85,15 +205,20 @@ window.onload=function(){
 
     <div class="row border border-dark border-bottom-0 rounded m-2 mb-4 pb-4">
         <div class="col-md-6">
+            <div id="march">
             <h3 class=" pt-3 pb-3">March</h3>
             <a>test</a>
+            </div>
         </div>
     </div>
 
     <div class="row border border-dark border-bottom-0 rounded m-2 mb-4 pb-4">
         <div class="col-md-6">
+            <div id="april">
+
             <h3 class=" pt-3 pb-3">April</h3>
             <a>test</a>
+            </div>
         </div>
     </div>
 
